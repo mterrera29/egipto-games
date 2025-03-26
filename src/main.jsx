7ca986229485b4fd39ee32172620f76010d23627
mterrera29 <matiasterrera29@gmail.com>
2025-03-25 23:04:31 -0300

    finish

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import { initializeApp } from "firebase/app";
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import { initializeApp } from 'firebase/app';
+import './index.css';
+
+const API_KEY = import.meta.env.VITE_API_KEY;
+const API_ID = import.meta.env.VITE_APP_ID;
+const MEASUREMENT_ID = import.meta.env.VITE_MEASUREMENT_ID;
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB9Hkxxe1f8gYAq-sqOZ3qX1xY5THflq2k",
-  authDomain: "mesopotamia-36df5.firebaseapp.com",
-  projectId: "mesopotamia-36df5",
-  storageBucket: "mesopotamia-36df5.appspot.com",
-  messagingSenderId: "882257115270",
-  appId: "1:882257115270:web:3e4ebeadb14659e1f5b907",
-  measurementId: "G-VK1KXEE9MV"
+  apiKey: API_KEY,
+  authDomain: 'mesopotamia-36df5.firebaseapp.com',
+  projectId: 'mesopotamia-36df5',
+  storageBucket: 'mesopotamia-36df5.appspot.com',
+  messagingSenderId: '882257115270',
+  appId: API_ID,
+  measurementId: MEASUREMENT_ID,
 };
 
 // Initialize Firebase
 initializeApp(firebaseConfig);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
